c9520415f82715b681ccf12eb027fd7d6458e9c7
Levon Aslanyan <aslanyan2012@mail.ru>
2024-02-07 15:56:40 +0400

    trainer data finished

--- src/components/Trainers/index.jsx
+++ src/components/Trainers/index.jsx
@@ -1,5 +1,28 @@
-const Trainers = () => {
-  return <div>Trainers</div>;
+// import TrainerImg from "../../assets/img/trainer_img.webp";
+import styles from "./index.module.css";
+import PropTypes from "prop-types";
+
+const Trainers = ({ name, company, img }) => {
+  return (
+    <div className={styles.section_trainer}>
+      <div className={styles.section_trainer_img}>
+        <img src={img} alt="Trainer" />
+      </div>
+      <p className={styles.section_trainer_info}>
+        <span>Name: </span>
+        {name}
+      </p>
+      <p className={styles.section_trainer_info}>
+        <span>GYM: </span> {company}
+      </p>
+    </div>
+  );
+};
+
+Trainers.propTypes = {
+  name: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  img: PropTypes.any.isRequired,
 };
 
 export default Trainers;
